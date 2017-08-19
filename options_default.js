19a7e6f83cf50762bc466ecfcab6f8a01646be7a
masashi.g <masashi.g@gmail.com>
2017-08-19 11:02:59 +0900

    add fullpath for ubutu

--- options_default.js
+++ options_default.js
@@ -13,12 +13,15 @@ module.exports =
   mcuOptions : {
     autoStartHeadless : true,
     headlessFullpath: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome', // for MacOS X
+    //headlessFullpath: '/usr/bin/chromium-browser', // for ubuntu + chromium
+    //headlessFullpath: '/usr/bin/google-chrome-stable', // for ubuntu + chrome
+
     headlessArgs: ['--headless',  '--disable-gpu',  '--remote-debugging-port=9222'], // With Debug port
-    //headlessArgs: ['--disable-gpu',  '--remote-debugging-port=9222'], // Without Debug prot
+    //headlessArgs: ['--disable-gpu',  '--remote-debugging-port=9222'], // NOT headless
+
     headlessUrlSingle: 'single_mcu.html',
-    headlessUrlMulti: 'multi_mcu.html',
+
     // NOT Supported yet :: maxUserInRoom: 4,
-    // NOT Supported yet :: maxRooms: 2
     dummyTail : false
   }
 }
